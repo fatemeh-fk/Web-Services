364c5233e835e94a2f42bc2afa1c01ebcc58bf1a
FATEMEH KARIMI <fatemeh.ka84@gmail.com>
2019-09-10 22:39:04 -0400

    -change the style of button -function edit

--- PrjApiExample/PrjApiExample/static/js/main.js
+++ PrjApiExample/PrjApiExample/static/js/main.js
@@ -4,7 +4,11 @@
         'DeleteUsers': 'https://my-json-server.typicode.com/wallouf/json-fake-server-test/users',
         'EditUsers': 'https://my-json-server.typicode.com/wallouf/json-fake-server-test/users'
     }
-
+function render(newuser) {
+    var i = 0;
+    $('#myTable').append('<tr><td class="noedit idname" id="idname">' + newuser.id + '</td><td class="noedit fname" id="fname">' + newuser.firstname + '</td><td class="noedit lname" id="lname">' + newuser.lastname + '</td><td class="noedit bplace" id="bplace">' + newuser.birthplace + '</td><td><input type="submit" value="Edit" class="btn btn-primary Edit noedit" id="btnEdit" name="' + i + '" ></input><input type="submit" id=btnDelete value="Delete" class="btn btn-primary Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="CancleEdit btn btn-primary edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="btn btn-primary SaveEdit edit" name="' + i + '"></td></tr>');
+    i++
+}
 $(function () {
     
 var $container = $('#myTable');
@@ -14,11 +18,12 @@ var $container = $('#myTable');
     //load user
         return jQuery.get(urls.GetUsers, function (items) {
             console.log(items);
-            var i=0
+           
             items.forEach((item) => {
-               
-                $('#myTable').append('<tr><td class="noedit idname" id="idname">' + item.id + '<input class="edit idname"/></td><td class="noedit fname" id="fname">' + item.firstname + '<input class="edit fname"/></td><td class="noedit lname" id="lname">' + item.lastname + '<input class="edit lname"/></td><td class="noedit bplace" id="bplace">' + item.birthplace + '<input class="edit bplace"/></td><td><input type="submit" value="Edit" class="Edit noedit" id="btnEdit"  name="' + i + '"></input><input type="submit" id=btnDelete value="Delete" class="Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="CancleEdit edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="SaveEdit edit" name="' + i + '"></td></tr>');
-                i++;
+                //var i=0
+                //$('#myTable').append('<tr><td class="noedit idname" id="idname">' + item.id + '</td><td class="noedit fname" id="fname">' + item.firstname + '</td><td class="noedit lname" id="lname">' + item.lastname + '</td><td class="noedit bplace" id="bplace">' + item.birthplace + '</td><td><input type="submit" value="Edit" class="btn btn-primary Edit noedit" id="btnEdit"  name="' + i + '"></input><input type="submit" id=btnDelete value="Delete" class="btn btn-primary Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="btn btn-primary CancleEdit edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="btn btn-primary SaveEdit edit" name="' + i + '"></td></tr>');
+                //i++;
+                render(item);
             });
 
         });
@@ -32,9 +37,9 @@ var $container = $('#myTable');
 $(document).on('click', '#add', function () {
        
     var user = {
-        fname: $('#fname-').val(),
-        lname: $('#lname-').val(),
-        bplace: $('#bplace-').val(),
+        firstname: $('#fname-').val(),
+        lastname: $('#lname-').val(),
+        birthplace: $('#bplace-').val(),
     };
     $.ajax({
         type: 'POST',
@@ -42,9 +47,10 @@ $(document).on('click', '#add', function () {
         data: user,
         success: function (newuser) {
             console.log(newuser)
-            var i = 0;
-            $('#myTable').append('<tr><td class="noedit idname" id="idname">' + newuser.id + '<input class="edit idname"/></td><td class="noedit fname" id="fname">' + newuser.fname + '<input class="edit fname"/></td><td class="noedit lname" id="lname">' + newuser.lname + '<input class="edit lname"/></td><td class="noedit bplace" id="bplace">' + newuser.bplace + '<input class="edit bplace"/></td><td><input type="submit" value="Edit" class="Edit noedit" id="btnEdit" name="' + i + '" ></input><input type="submit" id=btnDelete value="Delete" class="Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="CancleEdit edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="SaveEdit edit" name="' + i + '"></td></tr>');
-            i++
+            //var i = 0;
+            //$('#myTable').append('<tr><td class="noedit idname" id="idname">' + newuser.id + '</td><td class="noedit fname" id="fname">' + newuser.firstname + '</td><td class="noedit lname" id="lname">' + newuser.lastname + '</td><td class="noedit bplace" id="bplace">' + newuser.birthplace + '</td><td><input type="submit" value="Edit" class="btn btn-primary Edit noedit" id="btnEdit" name="' + i + '" ></input><input type="submit" id=btnDelete value="Delete" class="btn btn-primary Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="CancleEdit btn btn-primary edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="btn btn-primary SaveEdit edit" name="' + i + '"></td></tr>');
+            //i++
+            render(newuser)
         }
 
     });
@@ -72,40 +78,71 @@ $(document).on('click', '#btnDelete', function () {
 });
 //edit
 $(document).on('click', '#btnEdit', function () {
+
+
     var $tr = $(this).parent().parent();
-    var $td = $tr.parent().siblings();
-    console.log($td);
-    $tr.find('td input.fname').val($tr.find('td.fname').html());
-    $tr.find('td input.lname').val($tr.find('td.lname').html());
-    $tr.find('td input.bplace').val($tr.find('td.bplace').html());
-    $tr.addClass('edit');
+    var $td = $tr.find("td");
+
+    var fname = $(this).parents("tr").find("td:eq(1)").html();
+    $(this).parents("tr").find("td:eq(1)").html('<input name="edit_name" id="fn" value="' + fname + '">');
+    var lname = $(this).parents("tr").find("td:eq(2)").html();
+    $(this).parents("tr").find("td:eq(2)").html('<input name="edit_name" id="ln" value="' + lname + '">');
+    var bplace = $(this).parents("tr").find("td:eq(3)").html();
+    $(this).parents("tr").find("td:eq(3)").html('<input name="edit_name"id="bp" value="' + bplace + '">');
+
     
-   
+    $td.addClass('edit');
+    
 });
 //cancle
 $(document).on('click', '#btnCancle', function () {
+
     var $tr = $(this).parent().parent();
-    $tr.removeClass('edit');
+    var $td = $tr.find("td");
+    var $fn = $td.find("#fn").val();
+    var $ln = $td.find("#ln").val();
+    var $bp = $td.find("#bp").val();
+    $(this).parents("tr").find("td:eq(1)").text($fn);
+    $(this).parents("tr").find("td:eq(2)").text($ln);
+    $(this).parents("tr").find("td:eq(3)").text($bp);
+    $td.removeClass('edit');
    
 });
 //save
 $(document).on('click', '#btnSave', function () {
+
     var $tr = $(this).parent().parent();
-    var order={
-        fname: $tr.find('td.fname').val(),
-            lname: $tr.find('td.fname').val(),
-        place: $tr.find('td.fname').val(),
+    var $td = $tr.find("td");
+    var $fn = $td.find("#fn").val();
+    var $ln = $td.find("#ln").val();
+    var $bp = $td.find("#bp").val();
+    $(this).parents("tr").find("td:eq(1)").text($fn);
+    $(this).parents("tr").find("td:eq(2)").text($ln);
+    $(this).parents("tr").find("td:eq(3)").text($bp);
+    console.log("$fn", $fn)
+    $td.removeClass('edit');
+    var user={
+        fname: $fn,
+        lname: $ln,
+        place: $bp
     }
+    console.log(user);
     var id = $(this).parent().siblings(":first").text();
+    console.log(id)
     $.ajax({
+        
         url: urls.EditUsers + "/" + id,
         method: 'PUT',
-        data:order,
-        success: function (neworder) {
+        data:user,
+        success: function (newuser) {
+            console.log("hhh");
             var i = 0;
             $('#myTable').append('<tr><td class="noedit idname" id="idname">' + newuser.id + '<input class="edit idname"/></td><td class="noedit fname" id="fname">' + newuser.fname + '<input class="edit fname"/></td><td class="noedit lname" id="lname">' + newuser.lname + '<input class="edit lname"/></td><td class="noedit bplace" id="bplace">' + newuser.bplace + '<input class="edit bplace"/></td><td><input type="submit" value="Edit" class="Edit noedit" id="btnEdit" name="' + i + '" ></input><input type="submit" id=btnDelete value="Delete" class="Delete" name="' + i + '"><input type="submit" id=btnCancle value="Cancle" class="CancleEdit edit" name="' + i + '"><input type="submit" id=btnSave value="Save" class="SaveEdit edit" name="' + i + '"></td></tr>');
             i++
 
-        }
+        },
+        error: function (e) {
+                alert("error");
+           }
     });
 });
